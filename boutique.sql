086377141f9b7ffe9543e096b4c5d02a91e72b4a
felix-hauger <felix.hauger@laplateforme.io>
2023-04-05 14:06:23 +0200

    boutique.sql, product table - added NULL as DEFAULT value for discount_id

--- boutique.sql
+++ boutique.sql
@@ -93,7 +93,7 @@ CREATE TABLE `product` (
     `updated_at` DATETIME DEFAULT NULL,
     `deleted_at` DATETIME DEFAULT NULL,
     `category_id` INT UNSIGNED NOT NULL,
-    `discount_id` INT UNSIGNED NOT NULL,
+    `discount_id` INT UNSIGNED DEFAULT NULL,
     PRIMARY KEY(`id`),
     CONSTRAINT `fk_category_product` FOREIGN KEY(`category_id`) REFERENCES `category` (`id`),
     CONSTRAINT `fk_discount_product` FOREIGN KEY(`discount_id`) REFERENCES `discount` (`id`)
