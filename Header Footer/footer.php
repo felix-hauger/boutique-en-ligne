08b85c6be103ee1bba43e8c0930d3be18488eb8b
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-05 11:51:23 +0200

    Ajout du Header et du Footer

---
+++ Header Footer/footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="Header Footer/footer.css" rel="stylesheet" type="text/css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <!-- <script async src="Header Footer/footer.js"></script>-->
+</head>
+
+<body>
+   <footer>
+      <div id="boxFooter">
+
+         <ul>
+            <a href="index.php">4-Saisons </a><br>
+            <a href="index.php">Boutique </a><br>
+            <a href="index.php">Panier </a><br>
+         </ul>
+
+         <ul>
+            <a href="index.php">Saisons </a><br>
+            <a href="index.php">Profil </a><br>
+            <a href="index.php">Nouveautés </a><br>
+         </ul>
+      </div>
+      <div id="contact">Nous Contacter :<br></div>
+      <div id="boxAddr">
+
+         <div> Feriale.Bourega@laplateforme.io</div>
+         <div> Felix.Hauger@laplateforme.io</div>
+         <div> Lucas.Ribard@laplateforme.io</div>
+      </div>
+
+      <div id="boxMentionLégales">
+         <a href="index.php"> Conditions générales de vente</a>
+         <a href="index.php"> Mentions Légales</a>
+      </div>
+
+      </div>
+   </footer>
+</body>
+
+</html>
---
+++ Header Footer/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Header Footer/header.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script async src="Header Footer/header.js"></script>
+</head>
+
+<body>
+    <header>
+        <div>
+            <a href="index.php">4-Saisons </a> &emsp;
+            <a href="Boutique.php">Boutique </a> &emsp;
+            <i class="fa-sharp fa-solid fa-magnifying-glass fa-xs" onclick="Recherche()" id="SearchIcon"></i>
+            <i class="fa-solid fa-square-minus fa-xs" onclick="Recherche()" id="SearchHide"></i>
+            <input id="SearchBar" placeholder="Rechercher"></input>
+        </div>
+        <div>
+            <a href="user.php"><i class="fa-solid fa-user"></i></a>&emsp;
+            <a href="panier.php"><i class="fa-solid fa-cart-shopping"></i></a>
+            
+        </div>
+    </header>
+    <!--<i class="fa-solid fa-play" onclick="SetSaisons()"></i> BT changement de saison  -->
+
+</body>
+
+</html>
