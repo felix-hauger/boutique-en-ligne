96bbf800cc55acb3bb486f51d04e3fd4ad0205af
felix-hauger <felix.hauger@laplateforme.io>
2023-04-05 15:49:47 +0200

    added DbConnection singleton class to get PDO connection to database

---
+++ app/config/DbConnection.php
@@ -0,0 +1,39 @@
+<?php
+
+class DbConnection
+{
+    private static ?PDO $_db = null;
+
+    private function __construct()
+    {
+        // singleton
+    }
+
+    public static function getDb()
+    {
+        if (!self::$_db) {
+            try {
+                // get database infos from ini file in config folder
+                $db = parse_ini_file('db.ini');
+
+                // define PDO dsn with retrieved data
+                self::$_db = new PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=' . $db['charset'], $db['user'], $db['password']);
+
+                // prevent emulation of prepared requests
+                self::$_db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+        return self::$_db;
+    }
+}
+
+// how to use : 
+// $sql = 'SELECT id, name, description FROM product';
+
+// $select = DbConnection::getDb()->prepare($sql);
+
+// if ($select->execute()) {
+//     var_dump($select->fetchAll(PDO::FETCH_ASSOC));
+// }
