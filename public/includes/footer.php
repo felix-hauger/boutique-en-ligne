951eca8497fb317045032e7bb8ce87dbd69bba77
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-11 10:08:50 +0200

    Bugfix : fix liens css js

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -2,7 +2,7 @@
 
 <head>
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="Header Footer/footer.css" rel="stylesheet" type="text/css" />
+   <link href="includes/footer.css" rel="stylesheet" type="text/css" />
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
       integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
       crossorigin="anonymous" referrerpolicy="no-referrer" />
