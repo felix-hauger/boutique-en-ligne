fd778c9258b30405e47849f263da0ac105aff572
felix-hauger <felix.hauger@laplateforme.io>
2023-04-12 16:17:25 +0200

    PHP, controller/User.php - added comments + phpdoc comments

--- src/controller/User.php
+++ src/controller/User.php
@@ -8,17 +8,33 @@ use Exception;
 
 class User
 {
-    public function register(string $login, string $password, string $password_confirm, string $email, string $username, string $firstname, string $lastname)
+
+    /**
+     * @param string $login to auth
+     * @param string $password to auth, do not store in session
+     * @param string $email address personal info, can be used to send emails
+     * @param string $username visible to other users
+     * @param string $firstname personal info
+     * @param string $lastname personal info
+     * 
+     * @return bool when User model uses create method, depending if request is successfull or not
+     */
+    public function register(string $login, string $password, string $password_confirm, string $email, string $username, string $firstname, string $lastname): mixed
     {
         // register user with User model & entity
+        // instanciate User model
         $user_model = new UserModel();
+
+        // ! must use methods to test inputs
 
         if ($password === $password_confirm) {
             try {
+                // instanciate User entity
                 $user_entity = new UserEntity();
 
                 $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 13]);
 
+                // hydrate User entity properties
                 $user_entity
                     ->setLogin($login)
                     ->setPassword($password)
@@ -27,17 +43,25 @@ class User
                     ->setFirstname($firstname)
                     ->setLastname($lastname);
 
-                $user_model->create($user_entity);
+                // send User entity to model to create use in database
+                // return bool depending if model successfully created user or not
+                return $user_model->create($user_entity);
             } catch (\PDOException $e) {
                 echo $e->getMessage();
-                var_dump($e);
+                // var_dump($e);
             }
         } else {
             throw new Exception('Le mot de passe et la confirmation doivent être identiques');
         }
     }
 
-    public function connect(string $login, string $password)
+    /**
+     * @param string $login to auth
+     * @param string $password to auth, do not store in session
+     * 
+     * @return true if user infos are retrieved using User model find($id) method
+     */
+    public function connect(string $login, string $password): mixed
     {
         // retrieve user infos, hydrate User entity & store it in session
 
@@ -52,8 +76,10 @@ class User
             // var_dump($db_user);
 
             if (password_verify($password, $db_user['password']) || $password === $db_user['password']) {
+                // instanciate User entity
                 $user_entity = new UserEntity();
 
+                // hydrate User entity properties
                 $user_entity
                     ->setId($db_user['id'])
                     ->setLogin($db_user['login'])
@@ -64,12 +90,16 @@ class User
                     ->setRoleId($db_user['role_id']);
 
                 if (session_status() === PHP_SESSION_ACTIVE) {
+                    // store entity in session to use its structure & methods
                     $_SESSION['user'] = $user_entity;
                 }
+
+                // return true & stop executing method
                 return true;
             }
         }
 
+        // generic exception
         throw new \Exception('identifiants incorrects');
     }
 
@@ -93,7 +123,7 @@ class User
 
     /**
      * @param int id representing user id
-     * @return bool depending if model successfully deleted or not
+     * @return bool depending if model successfully deleted user or not
      */
     public function delete($id): bool
     {
