fe28f1d18193d6570629a2e015f592b7d2cf7df5
felix-hauger <felix.hauger@laplateforme.io>
2023-04-12 10:10:33 +0200

    PHP, controller/User.php::register() - added code with try..catch to register user

--- src/controller/User.php
+++ src/controller/User.php
@@ -10,6 +10,26 @@ class User
     public function register(string $login, string $password, string $password_confirm, string $email, string $username, string $firstname, string $lastname)
     {
         // register user with User model & entity
+        $user_model = new UserModel();
+
+        try {
+            $user_entity = new UserEntity();
+
+            $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 13]);
+
+            $user_entity
+                ->setLogin($login)
+                ->setPassword($password)
+                ->setEmail($email)
+                ->setUsername($username)
+                ->setFirstname($firstname)
+                ->setLastname($lastname);
+
+            $user_model->create($user_entity);
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            var_dump($e);
+        }
     }
 
     public function connect(string $login, string $password)
