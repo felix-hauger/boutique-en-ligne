35d100866d657ee57684016b7635e797a1b472bc
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-12 13:56:46 +0200

    Header Commun

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-
-<head>
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="includes/footer.css" rel="stylesheet" type="text/css" />
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
-   <!-- <script async src="includes/footer.js"></script>-->
-</head>
-
 <body>
    <footer>
       <div id="boxFooter">
