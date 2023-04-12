69cfcb1b3bbb1ff0097bc9fbfda13db84a029bd8
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-12 14:02:58 +0200

    new head in product.php

--- public/product.php
+++ public/product.php
@@ -31,12 +31,31 @@ $Cat = $Cat[0];
 <html lang="en">
 
 <head>
+    <!-- MetaData -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- CSS -->
+    <link href="includes/header.css" rel="stylesheet" type="text/css" />
+    <link href="includes/footer.css" rel="stylesheet" type="text/css" />
     <link href="style/product.css" rel="stylesheet" type="text/css" />
-    <script async src="scripts/product.js"></script>
+
+    <!-- FontAwesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- GoogleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script async src="includes/header.js"></script>
+
     <title><?= $result['name'] ?></title>
+
 </head>
 
 <body>
