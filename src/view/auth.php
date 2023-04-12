ea61842cd473fa5f1c92917b9bccb169b155095d
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-12 14:39:11 +0200

    CSS User Form

--- src/view/auth.php
+++ src/view/auth.php
@@ -2,23 +2,24 @@
     <i id="close-popup" class="fa-sharp fa-solid fa-xmark fa-2xl" onclick="HideUser()"></i><br>
 
     <form id="login-form" method="post">
+        <label class="LabelForm">Connexion</label><br>
         <input type="text" name="login" id="login" placeholder="Login"><br>
         <input type="password" name="password" id="password" placeholder="Mot de passe"><br>
-        <input type="submit" name="submit-login" value="Connexion">
+        <input class="BtSubmit" type="submit" name="submit-login" value="Connexion">
     </form>
-    
+
     <form id="register-form" method="post" style="display: none;">
+        <label class="LabelForm">Inscription</label><br>
         <input type="text" name="login" id="login" placeholder="Login"><br>
         <input type="password" name="password" id="password" placeholder="Mot de passe"><br>
         <input type="password" name="confirmation" id="confirmation" placeholder="Confirmation"><br>
         <input type="email" name="email" id="email" placeholder="Email"><br>
         <input type="text" name="firstname" id="firstname" placeholder="Prénom"><br>
         <input type="text" name="lastname" id="lastname" placeholder="Nom de famille"><br>
-        <input type="submit" name="submit-register" value="Inscription">
+        <input class="BtSubmit" type="submit" name="submit-register" value="Inscription">
     </form>
 
     <hr>
 
-    <button onclick="toggleAuth(event)">Pas de compte ?</button>
+    <button id="BtToggleConnec" onclick="toggleAuth(event)">Pas de compte ?</button>
 </div>
-
