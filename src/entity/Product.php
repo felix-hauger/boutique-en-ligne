7a573364c59b33ca0228b0dcf8c9fe75bb6f7694
felix-hauger <felix.hauger@laplateforme.io>
2023-04-14 11:06:56 +0200

    PHP, entity/Product.php - added setters & getters

--- src/entity/Product.php
+++ src/entity/Product.php
@@ -66,4 +66,220 @@ class Product
      * @var int representing the foreign key discount_id 
      */
     private int $_discount_id;
+
+    /**
+     * Get the value of _id
+     */
+    public function getId(): int
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Set the value of _id
+     */
+    public function setId(int $_id): self
+    {
+        $this->_id = $_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _name
+     */
+    public function getName(): string
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Set the value of _name
+     */
+    public function setName(string $_name): self
+    {
+        $this->_name = $_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _slug
+     */
+    public function getSlug(): string
+    {
+        return $this->_slug;
+    }
+
+    /**
+     * Set the value of _slug
+     */
+    public function setSlug(string $_slug): self
+    {
+        $this->_slug = $_slug;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _description
+     */
+    public function getDescription(): string
+    {
+        return $this->_description;
+    }
+
+    /**
+     * Set the value of _description
+     */
+    public function setDescription(string $_description): self
+    {
+        $this->_description = $_description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _price
+     */
+    public function getPrice(): int
+    {
+        return $this->_price;
+    }
+
+    /**
+     * Set the value of _price
+     */
+    public function setPrice(int $_price): self
+    {
+        $this->_price = $_price;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _image
+     */
+    public function getImage(): string
+    {
+        return $this->_image;
+    }
+
+    /**
+     * Set the value of _image
+     */
+    public function setImage(string $_image): self
+    {
+        $this->_image = $_image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _quantity_sold
+     */
+    public function getQuantitySold(): int
+    {
+        return $this->_quantity_sold;
+    }
+
+    /**
+     * Set the value of _quantity_sold
+     */
+    public function setQuantitySold(int $_quantity_sold): self
+    {
+        $this->_quantity_sold = $_quantity_sold;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _created_at
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->_created_at;
+    }
+
+    /**
+     * Set the value of _created_at
+     */
+    public function setCreatedAt(DateTime $_created_at): self
+    {
+        $this->_created_at = $_created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _updated_at
+     */
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->_updated_at;
+    }
+
+    /**
+     * Set the value of _updated_at
+     */
+    public function setUpdatedAt(DateTime $_updated_at): self
+    {
+        $this->_updated_at = $_updated_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _deleted_at
+     */
+    public function getDeletedAt(): DateTime
+    {
+        return $this->_deleted_at;
+    }
+
+    /**
+     * Set the value of _deleted_at
+     */
+    public function setDeletedAt(DateTime $_deleted_at): self
+    {
+        $this->_deleted_at = $_deleted_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _category_id
+     */
+    public function getCategoryId(): int
+    {
+        return $this->_category_id;
+    }
+
+    /**
+     * Set the value of _category_id
+     */
+    public function setCategoryId(int $_category_id): self
+    {
+        $this->_category_id = $_category_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _discount_id
+     */
+    public function getDiscountId(): int
+    {
+        return $this->_discount_id;
+    }
+
+    /**
+     * Set the value of _discount_id
+     */
+    public function setDiscountId(int $_discount_id): self
+    {
+        $this->_discount_id = $_discount_id;
+
+        return $this;
+    }
 }
