6ef6a1ad811a55eee59303b3b9f85c9d6fb5d7e1
felix-hauger <felix.hauger@laplateforme.io>
2023-04-17 15:48:14 +0200

    PHP, controller/Product - renamed find() method in get() & put code inside a try..catch block

--- src/controller/Product.php
+++ src/controller/Product.php
@@ -9,33 +9,37 @@ use DateTime;
 
 class Product extends AbstractController
 {
-    public function find(int $id)
+    public function get(int $id)
     {
         $product_model = new ProductModel();
-        $product_entity = new ProductEntity();
-
-        $db_product = $product_model->find($id);
-
-        // var_dump($db_product);
-        // isset($db_product['updated_at']) ? new DateTime($db_product['updated_at']): null;
-
-        $product_entity
-            ->setId($db_product['id'])
-            ->setName($db_product['name'])
-            ->setDescription($db_product['description'])
-            ->setPrice($db_product['price'])
-            ->setImage($db_product['image'])
-            ->setQuantitySold($db_product['quantity_sold'])
-            ->setCreatedAt(new DateTime($db_product['created_at']))
-            ->setUpdatedAt(isset($db_product['updated_at']) ? new DateTime($db_product['updated_at']): null)
-            ->setDeletedAt(isset($db_product['deleted_at']) ? new DateTime($db_product['deleted_at']): null)
-            ->setCategoryId($db_product['category_id'])
-            ->setCategoryName($db_product['category_name']);
-
-        return $product_entity;
+        
+        try {
+            $db_product = $product_model->find($id);
+            
+            // var_dump($db_product);
+            // isset($db_product['updated_at']) ? new DateTime($db_product['updated_at']): null;
+            $product_entity = new ProductEntity();
+    
+            $product_entity
+                ->setId($db_product['id'])
+                ->setName($db_product['name'])
+                ->setDescription($db_product['description'])
+                ->setPrice($db_product['price'])
+                ->setImage($db_product['image'])
+                ->setQuantitySold($db_product['quantity_sold'])
+                ->setCreatedAt(new DateTime($db_product['created_at']))
+                ->setUpdatedAt(isset($db_product['updated_at']) ? new DateTime($db_product['updated_at']): null)
+                ->setDeletedAt(isset($db_product['deleted_at']) ? new DateTime($db_product['deleted_at']): null)
+                ->setCategoryId($db_product['category_id'])
+                ->setCategoryName($db_product['category_name']);
+    
+            return $product_entity;
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
 
 $p = new Product();
-var_dump($p->find(28));
+var_dump($p->get(80));
 
