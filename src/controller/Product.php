ae7f749c80504e2a937a4c1a32af62bd0ddc7bda
felix-hauger <felix.hauger@laplateforme.io>
2023-04-18 14:40:55 +0200

    PHP, controller/Product - added checkSeason() method to get current season

--- src/controller/Product.php
+++ src/controller/Product.php
@@ -10,7 +10,7 @@ use Exception;
 
 class Product extends AbstractController
 {
-    public function get(int $id)
+    public function get(int $id): ProductEntity
     {
         $product_model = new ProductModel();
         
@@ -39,8 +39,39 @@ class Product extends AbstractController
             throw new Exception($e->getMessage());
         }
     }
+
+    /**
+     * @return string The season name
+     * mars - mai : printemps
+     * juin - août : été
+     * septembre - novembre : automne
+     * décembre - février : hiver
+     */
+    public function checkSeason(): string
+    {
+        $date_time = new \DateTime('12-10-2020');
+
+        $month = $date_time->format('n');
+
+        switch (true) {
+            case $month > 2 && $month < 6:
+                return 'printemps';
+
+            case $month < 9:
+                return 'été';
+
+            case $month < 12:
+                return 'automne';
+            
+            default:
+                return 'hiver';
+        }
+    }
+
 }
 
 // $p = new Product();
+// echo $p->checkSeason();
+// $p->index();
 // var_dump($p->get(80));
 
