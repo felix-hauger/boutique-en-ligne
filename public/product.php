dc15442ee009947d5d845b0e86624d408f9ac9b6
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-19 10:06:04 +0200

    jquery not needed

--- public/product.php
+++ public/product.php
@@ -90,7 +90,6 @@ if ($select->execute()) {
 
     <!-- Scripts -->
     <script async src="includes/header.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script async src="scripts/product.js"></script>
 
     <title>
