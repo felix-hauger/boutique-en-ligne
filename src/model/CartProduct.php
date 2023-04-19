867f2944f8c5f7f2e3385c859f960e3aab67a824
felix-hauger <felix.hauger@laplateforme.io>
2023-04-19 15:30:24 +0200

    PHP, model/CartProduct - type methods params & return values + added phpdoc comments

--- src/model/CartProduct.php
+++ src/model/CartProduct.php
@@ -14,14 +14,23 @@ class CartProduct
      */
     private ?PDO $_pdo = null;
 
+    /**
+     * Initialize database connection
+     */
     public function __construct()
     {
         $this->_pdo = DbConnection::getPdo();
     }
 
-    public function create($cart_id, $product_id, $product_quantity)
+    /**
+     * @param int $cart_id Part 1 of the primary key, the cart id foreign key
+     * @param int $product_id Part 2 of the primary key, the product id foreign key
+     * @param int $product_quantity The quantity of the product
+     * @return bool Depending if the request is executed successfully
+     */
+    public function create(int $cart_id, int $product_id, int $product_quantity): bool
     {
-        // récupère les id cart & product, insert la quantité du product
+        // fetch cart & product ids, insert them as PRIMARY KEY with product quantity
         $sql = 'INSERT INTO cart_product (cart_id, product_id, product_quantity) VALUES (:cart_id, :product_id, :product_quantity)';
 
         $insert = $this->_pdo->prepare($sql);
@@ -33,9 +42,14 @@ class CartProduct
         return $insert->execute();
     }
 
-    public function find($cart_id, $product_id)
+    /**
+     * @param int $cart_id Part 1 of the primary key, the cart id foreign key
+     * @param int $product_id Part 2 of the primary key, the product id foreign key
+     * @return array|false SQL result if the request is executed successfully
+     */
+    public function find(int $cart_id, int $product_id): array|false
     {
-        // récupère les infos du cart avec la clé primaire (id cart & product)
+        // fetch cart infos with PRIMARY KEY (cart_id, product_id)
         $sql = 'SELECT * FROM cart_product WHERE cart_id = :cart_id AND product_id = :product_id';
 
         $select = $this->_pdo->prepare($sql);
@@ -48,9 +62,14 @@ class CartProduct
         return $select->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function update($cart_id, $product_id, $product_quantity)
+    /**
+     * @param int $cart_id Part 1 of the primary key, the cart id foreign key
+     * @param int $product_id Part 2 of the primary key, the product id foreign key
+     * @param int $product_quantity The quantity of the product
+     * @return bool Depending if the request is executed successfully
+     */
+    public function update(int $cart_id, int $product_id, int $product_quantity): bool
     {
-        // update product quantity (? delete if quantity is 0 ?)
         $sql = 'UPDATE cart_product SET product_quantity = :product_quantity WHERE cart_id = :cart_id AND product_id = :product_id';
 
         $update = $this->_pdo->prepare($sql);
@@ -62,10 +81,15 @@ class CartProduct
         return $update->execute();
     }
 
-    public function delete($cart_id, $product_id)
+    /**
+     * @param int $cart_id Part 1 of the primary key, the cart id foreign key
+     * @param int $product_id Part 2 of the primary key, the product id foreign key
+     * @return bool Depending if the request is executed successfully
+     */
+    public function delete(int $cart_id, int $product_id): bool
     {
-        // delete en utilisant la clé primaire (mix des clés étrangères)
-        // update product quantity (? delete if quantity is 0 ?)
+        // delete using primary key (foreign keys mix)
+        // ? delete if product_quantity is 0?
         $sql = 'DELETE FROM cart_product WHERE cart_id = :cart_id AND product_id = :product_id';
 
         $insert = $this->_pdo->prepare($sql);
