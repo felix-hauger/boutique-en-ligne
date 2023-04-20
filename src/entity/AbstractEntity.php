813b07b2e15f7c03781bcae9cbd660b7d4a44e42
felix-hauger <felix.hauger@laplateforme.io>
2023-04-20 16:59:53 +0200

    PHP, entity/AbstractEntity::hydrate() - use ReflectionClass to test if property type is DateTime, if so instanciate new DateTime() using datetime string retrieved from database

--- src/entity/AbstractEntity.php
+++ src/entity/AbstractEntity.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use DateTime;
+use ReflectionClass;
+
 abstract class AbstractEntity
 {
     /**
@@ -44,14 +47,29 @@ abstract class AbstractEntity
      */
     public function hydrate(array $data): void
     {
+        // Get infos from instanciated class
+        $reflection = new ReflectionClass($this);
+
         foreach ($data as $key => $value) {
             // Get formatted method name by converting underscores in spaces,
             // make first letter of each word uppercase, remove all spaces,
-            // then concatenate it with 'set' to get full method name
-            $method = 'set' . str_replace(' ', '', ucwords(str_replace('_', ' ', $key))) ;
+            // then concatenate it with 'set' to get full setter method name
+            $setter = 'set' . str_replace(' ', '', ucwords(str_replace('_', ' ', $key))) ;
 
-            if (is_callable([$this, $method])) {
-                $this->$method($value);
+            if (is_callable([$this, $setter])) {
+                // Entities class properties start by '_'
+                $property = '_' . $key;
+
+                // Use ReflectionClass to test if property data type is a DateTime
+                if ($reflection->getProperty($property)->getType()->getName() === 'DateTime') {
+                    // If value is a string (not null)
+                    if (is_string($value)) {
+                        // Create DateTime using string value
+                        $value = new DateTime($value);
+                    }
+                }
+
+                $this->$setter($value);
             }
         }
     }
