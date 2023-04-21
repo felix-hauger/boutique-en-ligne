f6870a7f5d22fdd84afde69fc72f17fab3821a88
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-21 11:55:24 +0200

    CSS change on admin.php

--- src/view/admin_articles.php
+++ src/view/admin_articles.php
@@ -1,1 +1,40 @@
-articles
+<?php
+
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use App\Config\DbConnection;
+
+
+$sql = "SELECT * FROM product";
+$select = DbConnection::getPdo()->prepare($sql);
+if ($select->execute()) {
+    $products = $select->fetchall(\PDO::FETCH_ASSOC);
+}
+echo '<table class="TBAffichage">';
+
+foreach ($products as $product) {
+
+
+    echo '<tr>';
+    echo '<td>';
+    echo 'Produit : ' . $product['name'];
+    echo '</td>';
+
+    echo '<td>';
+    echo 'Prix : ' . $product['price'];
+    echo '</td>';
+
+
+    echo '<td>';
+    echo 'E-Mail : ' . $product['quantity_sold'];
+    echo '</td>';
+
+   
+    echo '<td><button>modifier l\'Article</button></td>';
+    echo '<td><button class="Supprimer">Supprimer l\'user</button></td>';
+
+
+}
+echo "</table>";
+?>
