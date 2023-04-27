c8de138b215a2b417a83e615dbd00ac8cc16242f
lucas-ribard <lucas.ribard@laplateforme.io>
2023-04-27 11:27:36 +0200

    Added user page to view user info

---
+++ public/user.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- MetaData -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!-- FontAwesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- GoogleFont-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link href="style/user.css" rel="stylesheet" type="text/css" />
+    <link href="includes/header.css" rel="stylesheet" type="text/css" />
+    <link href="includes/footer.css" rel="stylesheet" type="text/css" />
+
+    <!-- Scripts -->
+    <script async src="includes/header.js"></script>
+    <script async src="scripts/user.js"></script>
+
+    <title>Panier | Saisons à la mode</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
