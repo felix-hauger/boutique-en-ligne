2064159d41af17f721eb02c7345cd9db3a6cf1f8
felix-hauger <felix.hauger@laplateforme.io>
2023-04-28 12:55:38 +0200

    PHP, public/cart.php - fixed display value for cookie cart + removed old commented code

--- public/cart.php
+++ public/cart.php
@@ -142,48 +142,13 @@ $TOTAL = [];
                         echo CartController::toHtmlCookieItem($item);
 
                         if ($item['quantity'] > 1) {
-                            $Nprice = $item['price'] * $val;
+                            $Nprice = $item['price'] * $item['quantity'];
                             array_push($TOTAL, [$item['name']."<b> x ".$item['quantity']."</b>", $Nprice]);
                         } else {
                             array_push($TOTAL, [$item['name'], $item['price']]);
                         }
                     }
                 }
-
-                // foreach ($_COOKIE as $key => $val) {
-                     
-                //     if (substr($key, 0, 7) == "product") {
-                //         // le cookie est divisé en plusieures partie    product(string)  id   taille  quantité
-                //         list($ignore, $product_id, $size) = explode("_", $key);
-                //         //pour etre sur que l'on récupere que les cookie de notre panier
-                
-                //         //on récupere les info de l'article 
-                //         $product = GetProduct($product_id);
-                //         echo '<tr>';
-                //         echo "<td class='Tdbg' rowspan='2' class='TdImage'><img class='imageCart' src='" . $product['image'] . "'></td>";
-                //         echo "<td class='Tdbg' ><b>" . $product['name'] . "</b></td>";
-                //         echo "<td class='Tdbg' >Taille : <b>" . strtoupper($size) . "</b></td>";
-                //         echo "<td class='Tdbg' >Quantité : <b>" . $val . "</b></td>";
-
-                //         if ($val > 1) {
-                //             $Nprice = $product['price'] * $val;
-                //             array_push($TOTAL, [$product['name']."<b> x ".$val."</b>", $Nprice]);
-                //             echo "<td class='Tdbg' ><b>" . $Nprice . "€</b><br>";
-                //             echo "(" . $product['price'] . "€ x " . $val . ")";
-                //             echo "</td>";
-                //         } else {
-                //             echo "<td class='Tdbg' ><b>" . $product['price'] . "€</b></td>";
-                //             array_push($TOTAL, [$product['name'], $product['price']]);
-                //         }
-
-                //         echo '</tr>';
-
-                //         echo '<tr>';
-                //         echo "<td class='TdDesc' colspan='3'>" . $product['description'] . "</td>";
-                //         echo "<td class='BoxBtSuppCookie'><button class='BtSuppCookie' onclick='SupprimerCookie(\"" . $key . "\")'>Supprimer L'article</button></td>";
-                //         echo '</tr>';
-                //     }
-                // }
 
                 ?>
             </table>
